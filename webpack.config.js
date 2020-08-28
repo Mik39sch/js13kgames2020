8dca6e8657028b06b497f80a5a42a6d9d818cca0
mluck <mlucky39sch@gmail.com>
2020-08-28 23:10:29 +0900

    terser導入

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,25 @@
+const TerserPlugin = require('terser-webpack-plugin');
 module.exports = {
     mode: 'production',
     entry: './js/app.js',
     output: {
       path: __dirname + '/dist',
       filename: 'bundle.min.js'
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin({
+        terserOptions: {
+          ecma: 6,
+          compress: true,
+          output: {
+            comments: false,
+            beautify: false
+          }
+        }
+      })]
     }
-  };
+};
+// module.exports = {
+
+//   };
